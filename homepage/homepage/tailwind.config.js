68dc5761c4ef931be740681f61bab1b79bca017d
shahardagancbamoo <shahar@cbamboo.com>
2024-11-17 07:31:46 +0000

    First version of home page done.

---
+++ homepage/homepage/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#61CDB8", // Using your app's color scheme
+        secondary: "#F47560",
+      },
+    },
+  },
+  plugins: [],
+};
